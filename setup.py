69064aa98d978895f234508cc30f0cf9d31ea20b
FireHead90544 <rudranshjoshi1806@gmail.com>
2021-02-15 20:31:48 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.rst", "r") as fh:
+    readme = fh.read()
 
 setup(name='mathemagic',
       version='1.0',
       description='Module containing a lot of general and commonly used mathematical functions.',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
+      long_description=readme,
+      long_description_content_type="text/x-rst",
       url='http://github.com/FireHead90544/mathemagic',
+      project_urls={
+        "Documentation": "https://mathemagic.readthedocs.io/en/latest/",
+        "Issue tracker": "https://github.com/FireHead90544/mathemagic/issues",
+      },
       author='Rudransh Joshi',
       author_email='rudranshjoshi1806@gmail.com',
       license='GNU GPL v3',
